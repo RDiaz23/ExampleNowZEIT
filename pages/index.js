73b9641f1c957403cc9fb44ff64fea7851c0b0e1
Roger <thekiller23@live.com.mx>
2018-12-08 07:40:40 -0500

    complete

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,31 @@
-import React, { PureComponent } from 'react'
-import Link from 'next/link'
 import 'isomorphic-fetch'
+import Layout from '../components/Layout'
+import ChannelGrid from '../components/ChannelGrid'
+import Error from './_error'
 
-export default class extends PureComponent {
+export default class extends React.Component {
 
-  static async getInitialProps(){
+  static async getInitialProps({ res }) {
+    try {
       let req = await fetch('https://api.audioboom.com/channels/recommended')
-      let {body: channels} = await req.json()
-
-      return { channels }
-  }  
-  render() {
-    const {channels} = this.props;
-    return  <div>
-        <header>Podcast</header>
-        
-        <div className="channels">
-            { channels.map((channel) => (
-              <Link href={`/channel?id=${channel.id}`} prefetch>
-                <a className="channel" key={channel.id.toString()}>
-                    <img src={channel.urls.logo_image.original}/>
-                    <h2>{channel.title}</h2>
-                </a>
-              </Link>
-            )) }
-        </div>
-      <style jsx>{`
-        header {
-          color: #fff;
-          background: #8756ca;
-          padding: 15px;
-          text-align: center;
-        }
-        .channels {
-          display: grid;
-          grid-gap: 15px;
-          padding: 15px;
-          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-        }
-        a.channel {
-          display: block;
-          margin-bottom: 0.5em;
-          color: #333;
-          text-decoration: none;
-        }
-        .channel img {
-          border-radius: 3px;
-          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
-          width: 100%;
-        }
-        h2 {
-          padding: 5px;
-          font-size: 0.9em;
-          font-weight: 600;
-          margin: 0;
-          text-align: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        body {
-          margin: 0;
-          font-family: system-ui;
-          background: white;
-        }
-      `}</style>
-      </div>
-    
+      let { body: channels } = await req.json()
+      return { channels, statusCode: 200 }
+    } catch(e) {
+      res.statusCode = 503
+      return { channels: null, statusCode: 503 }
+    }
   }
+
+  render() {
+    const { channels, statusCode } = this.props
+
+    if( statusCode !== 200 ) {
+      return <Error statusCode={ statusCode } />
+    }
+
+    return <Layout title="Podcasts">
+      <ChannelGrid channels={ channels } />
+    </Layout>
+  }
+  
 }
